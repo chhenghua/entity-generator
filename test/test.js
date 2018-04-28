5ef32b499b643090f67a357baaa7244318d8abd1
chenghua <chenghuab@yonyou.com>
2018-04-28 11:26:30 +0800

    firt version

---
+++ test/test.js
@@ -0,0 +1,10 @@
+
+const generator = require('../lib');
+
+generator({
+    host: '172.30.3.206',
+    database: 'wk_egg_test',
+    username: 'wython',
+    password: 'b12345678',
+    path: '/work/WK-server-egg-ts/app/model'
+});
