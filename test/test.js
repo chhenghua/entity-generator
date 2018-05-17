936633e8c1ac1b1dc3603c0c9c04cf4a8aacc1a8
chenghua <chenghuab@yonyou.com>
2018-05-17 09:56:13 +0800

    字段排序

--- test/test.js
+++ test/test.js
@@ -2,9 +2,13 @@
 const generator = require('../lib');
 
 generator({
-    host: '172.30.3.206',
-    database: 'wk_egg_test',
-    username: 'wython',
-    password: 'b12345678',
-    path: '/work/WK-server-egg-ts/app/model'
+    host: '172.29.1.15',
+    database: 'wk',
+    username: 'root',
+    password: '123456',
+    path: '/work/WK-server-egg-ts/app/model',
+    sequelizeOpt: {
+        freezeTableName: true,
+        timestamps: false
+    }
 });
